9ba326db04358c233a242d279731ba282b87ec12
hendisantika <hendisantika@yahoo.co.id>
2026-07-17 07:57:58 +0700

    feat: add create_todo in repositories.py 🫸🌀✏️📗🐧🐳⬆

--- repositories.py
+++ repositories.py
@@ -23,3 +23,19 @@ def update_todo(db: Session, update_todo: models.Todo, todo_id: int):
         return todo
     else:
         return None
+
+def create_todo(db: Session, todo:schemas.Todo):
+    db_todo = models.Todo(title=todo.title,
+                          status=todo.status,
+                          created_at=datetime.now(),
+                          updated_at=datetime.now())
+    db.add(db_todo)
+    db.commit()
+    db.refresh(db_todo)
+    return db_todo
+
+
+# NOTE :
+# - To perform CRUD need to add object instance to the database session.
+# - do commit changes
+# - do refresh your instance to contain new data from the database.
