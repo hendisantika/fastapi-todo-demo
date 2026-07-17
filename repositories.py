694ebe1cc4bd80b7a2939913c5bf584c4b30e61d
hendisantika <hendisantika@yahoo.co.id>
2026-07-17 07:57:09 +0700

    feat: add get_todos in repositories.py 🫸🌀✏️📗🐧🐳⬆

--- repositories.py
+++ repositories.py
@@ -2,3 +2,6 @@ from sqlalchemy.orm import Session
 from datetime import datetime
 
 import models,schemas
+
+def get_todos(db: Session, skip:int=0, limit: int=100):
+    return db.query(models.Todo).offset(skip).limit(limit).all()
