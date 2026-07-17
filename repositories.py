b58581cda889bbf2a72d87e00cb4926ca9f1a1fb
hendisantika <hendisantika@yahoo.co.id>
2026-07-17 07:57:19 +0700

    feat: add get_todo in repositories.py 🫸🌀✏️📗🐧🐳⬆

--- repositories.py
+++ repositories.py
@@ -5,3 +5,6 @@ import models,schemas
 
 def get_todos(db: Session, skip:int=0, limit: int=100):
     return db.query(models.Todo).offset(skip).limit(limit).all()
+
+def get_todo(db: Session, todo_id: int):
+    return db.query(models.Todo).filter(models.Todo.id == todo_id).first()
