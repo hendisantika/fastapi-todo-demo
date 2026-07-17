eb873ae693e882b1b943a76dc95c139fc07d0e99
hendisantika <hendisantika@yahoo.co.id>
2026-07-17 07:57:30 +0700

    feat: add delete_todo in repositories.py 🫸🌀✏️📗🐧🐳⬆

--- repositories.py
+++ repositories.py
@@ -8,3 +8,6 @@ def get_todos(db: Session, skip:int=0, limit: int=100):
 
 def get_todo(db: Session, todo_id: int):
     return db.query(models.Todo).filter(models.Todo.id == todo_id).first()
+
+def delete_todo(db: Session, todo_id: int):
+    return db.query(models.Todo).filter(models.Todo.id == todo_id).delete()
