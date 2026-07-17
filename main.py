8380f34712ab77170eeb78b8931b9054a06bc30d
hendisantika <hendisantika@yahoo.co.id>
2026-07-17 07:58:46 +0700

    feat: add main.py 🫸🌀✏️📗🐧🐳⬆

---
+++ main.py
@@ -0,0 +1,11 @@
+from fastapi import FastAPI, APIRouter, Depends, HTTPException, status
+from pydantic import BaseModel
+from database import Base, SessionLocal
+from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy.orm import Session
+
+import models,schemas, repositories
+import logging
+
+#create FastAPI application instance
+app = FastAPI()
